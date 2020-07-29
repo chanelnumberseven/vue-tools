bb844dea825547ecadd07a44ab3a1f1fb471eb4e
erhai <erhaier_hai@126.com>
2020-07-29 16:52:48 +0800

    修改正则

--- utils/regular.js
+++ utils/regular.js
@@ -1,4 +1,5 @@
-export const email=/[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/g
-export const phoneNumber=/[0-9]{11,11}/g
+export const email=/[\w.]+@[\w.]+\.\w+/g
+export const phone=/[0-9]{11,11}/g
 export const websit=/[a-zA-z]+:\/\/[^\s]*/g
-export const phone=/\d{3}-\d{8}|\d{4}-\{7,8}/g
+export const landline=/\d{3,4}-\d{7,8}|\d{7,8}/g
+export const blank=/\s/g
