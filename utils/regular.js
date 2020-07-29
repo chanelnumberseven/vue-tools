808b43af23953aa7778455f25dd7b5859a38d6b5
erhai <erhaier_hai@126.com>
2020-07-29 15:44:03 +0800

    添加工具
    1 vue filter指令
    2 常用工具
    3 正则

---
+++ utils/regular.js
@@ -0,0 +1,4 @@
+export const email=/[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/g
+export const phoneNumber=/[0-9]{11,11}/g
+export const websit=/[a-zA-z]+:\/\/[^\s]*/g
+export const phone=/\d{3}-\d{8}|\d{4}-\{7,8}/g
